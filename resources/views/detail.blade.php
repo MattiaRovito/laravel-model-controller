77cba5bbd52132262eef1dcfcd4c25666507d950
MattiaR <rovitomattia91@gmail.com>
2021-09-07 19:16:24 +0200

    ultimo commit: aggiunta la descrizione

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,10 +4,13 @@
 @section('content')
     {{-- @dd($serie) --}}
     <div>
-        <h2>{{ $serie->title }} </h2>
+        <h2 class="white">{{ $serie->title }} </h2>
         
         <img src="{{ $serie['thumb']}}" alt="{{ $serie['title']}}">
         
+        <p class="white">
+            {{ $serie->description }}
+        </p>
     </div>
 @endsection
     
